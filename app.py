dae6b674844d515306e18af84024b73cf16c3e70
qaz91341 <s91341@gmail.com>
2022-06-04 16:39:06 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ def handle_message(event):
 
 
 #主程式
-import os if __name__ == "__main__":
+import os 
+if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
